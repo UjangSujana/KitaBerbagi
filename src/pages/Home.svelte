eea2e7e99644e4e692f54186fc269786abdd86c1
Ujang Sujana <zoelfasujana@gmail.com>
2020-08-11 18:25:48 +0700

    convert HTML to svelte component

---
+++ src/pages/Home.svelte
@@ -0,0 +1,19 @@
+<script>
+    import KitaBerbagi from "../component/KitaBerbagi.svelte";
+    import Header from '../component/Header.svelte';
+    import Wellcome from '../component/Wellcome.svelte';
+    import Promo from '../component/Promo.svelte';
+    import Footer from '../component/Footer.svelte';
+
+    import {charities} from "../Data/charities.js" ;
+
+    let title ="Kita Berbagi";
+</script>
+
+
+
+<Header />
+<Wellcome />
+<KitaBerbagi {charities} />
+<Promo />
+<Footer />
